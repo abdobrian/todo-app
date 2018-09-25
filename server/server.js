10669208ec32fa19d1ee8d71eb406babadc1bb73
Brian Abdo <fleenyworks@gmail.com>
2018-09-25 18:51:09 -0400

    Heroku Fix

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
+var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
 const {ObjectID} = require('mongodb');
 
