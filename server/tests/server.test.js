348c41e80d6c9abf3dd7c32bc8b908da97e74c77
Brian Abdo <fleenyworks@gmail.com>
2018-09-25 18:53:10 -0400

    Heroku Fix

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,7 +1,7 @@
 const expect = require('expect');
 const request = require('supertest');
 const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const {Todo} = require('./../models/Todo');
 
 const {ObjectID} = require('mongodb');
 
